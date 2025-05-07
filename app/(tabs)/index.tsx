fe2d9c9e0495f8424f03cbaaf4cc999ed85562cd
Damian Garasz <damian.garasz@gmail.com>
2025-05-07 22:14:21 +0200

    dziubie

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center flex-1 justify-center">
+      <Text className="color-red-900 text-8xl">DUPA</Text>
+    </View>
+  );
+}
